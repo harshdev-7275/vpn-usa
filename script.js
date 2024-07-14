67e27c9a1f04b5c2f81269d46588f2995d28d875
Harsh Singh <harsh.dev7275@gmail.com>
2024-07-14 18:39:15 +0530

    implemented carausal

---
+++ script.js
@@ -0,0 +1,33 @@
+const carouselInner = document.querySelector(".carousel-inner");
+const carouselItems = document.querySelectorAll(".carousel-item");
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+let currentIndex = 0;
+
+function updateCarousel() {
+  const width = carouselItems[0].offsetWidth * 2;
+  carouselInner.style.transform = `translateX(-${currentIndex * width}px)`;
+}
+
+function nextSlide() {
+  currentIndex =
+    currentIndex === Math.ceil(carouselItems.length / 2) - 1
+      ? 0
+      : currentIndex + 1;
+  updateCarousel();
+}
+
+function prevSlide() {
+  currentIndex =
+    currentIndex === 0
+      ? Math.ceil(carouselItems.length / 2) - 1
+      : currentIndex - 1;
+  updateCarousel();
+}
+
+prevButton.addEventListener("click", prevSlide);
+nextButton.addEventListener("click", nextSlide);
+
+window.addEventListener("resize", updateCarousel);
+updateCarousel(); 
+setInterval(nextSlide, 2000); 
