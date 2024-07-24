3ce38c61fdb54a7bed519e8f36e68cf32e63146c
harshdev-7275 <harsh.dev7275@gmail.com>
2024-07-24 16:32:43 +0530

    navbar

--- js/script.js
+++ js/script.js
@@ -31,4 +31,27 @@ nextButton.addEventListener("click", nextSlide);
 
 window.addEventListener("resize", updateCarousel);
 updateCarousel(); 
-setInterval(nextSlide, 2000); 
+setInterval(nextSlide, 4000); 
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const hamburger = document.getElementById('hamburger');
+  const navLinks = document.getElementById('nav-links-hamburger');
+
+  
+  function resetNavLinks() {
+    if (window.innerWidth > 667) {
+      navLinks.style.display = 'none';
+    }
+  }
+
+  hamburger.addEventListener('click', function() {
+    navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+    console.log(navLinks.style.display);
+    hamburger.classList.toggle('open');
+  });
+
+  window.addEventListener('resize', resetNavLinks);
+
+  resetNavLinks();
+});
